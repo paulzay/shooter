7124965c34805fb22cebfa0252fcfc2aeeef7f9e
paulzay <paulotieno2@gmail.com>
2020-10-31 16:52:08 +0300

    import new functions and run phaser

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import 'phaser';
+import config from './config/config';
+import GameScene from './scenes/gameScene';
+import BootScene from './scenes/bootScene';
+import PreloaderScene from './scenes/preloaderScene';
+import TitleScene from './scenes/titleScene';
+import OptionsScene from './scenes/optionsScene';
+import CreditsScene from './scenes/creditsScene';
+import Model from './Model';
+
+class Game extends Phaser.Game {
+  constructor () {
+    super(config);
+    const model = new Model();
+    this.globals = { model, bgMusic: null };
+    this.scene.add('Boot', BootScene);
+    this.scene.add('Preloader', PreloaderScene);
+    this.scene.add('Title', TitleScene);
+    this.scene.add('Options', OptionsScene);
+    this.scene.add('Credits', CreditsScene);
+    this.scene.add('Game', GameScene);
+    this.scene.start('Boot');
+  }
+}
+
+window.game = new Game();
