593628c710748483f8612f91a917eb0d3fb4ce6d
paulzay <paulotieno2@gmail.com>
2020-11-02 15:28:32 +0300

    load files and add game background

--- src/scenes/gameScene.js
+++ src/scenes/gameScene.js
@@ -6,10 +6,32 @@ export default class GameScene extends Phaser.Scene {
     }
 
     preload(){
-        this.load.image('logo', 'assets/logo.png');
+        this.load.image('space', 'assets/spacessets/space.svg');
+		this.load.image('laser', 'assets/spacessets/projectile1.svg');
+		this.load.spritesheet("ship","assets/spacessets/starship.svg", {
+        frameWidth: 16,
+        frameHeight: 16
+		});
+		this.load.spritesheet('foe1', 'assets/spacessets/starshipdark.svg', {
+        frameWidth: 16,
+        frameHeight: 16
+		});
+		this.load.image('foe2','assets/spacessets/ufo.svg')
+		this.load.spritesheet('boss','assets/spacessets/ufodark.svg', {
+        frameWidth: 16,
+        frameHeight: 16
+		})
+		this.load.image('asteroid1','assets/spacessets/asteroid1_brown.svg')
+		this.load.image('asteroid2','assets/spacessets/asteroid1_grey.svg')
+		this.load.image('asteroid3','assets/spacessets/asteroid2_brown.svg')
+		this.load.image('asteroid4','assets/spacessets/asteroid2_grey.svg')
+		this.load.image('torpedo','assets/spacessets/torpedo.svg')
+		this.load.image('torpedodark','assets/spacessets/torpedodark.svg')
+
     }
 
     create(){
-        this.add.image(400,300, 'logo');
-    }
+		this.add.image(400,300, 'space');
+
+	}
 }
