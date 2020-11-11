362cd9ac74a1e8ac5fde7b4a2c5f751ee41d9c8b
paulzay <paulotieno2@gmail.com>
2020-11-11 15:01:01 +0300

    initialize values to be used globally

---
+++ src/objects/GlobalSettings.js
@@ -0,0 +1,17 @@
+export const GlobalSettings = {
+  enemySpawnDelay: 2000, // ms
+  // Enemy Shooter
+  enemyShooterBulletDelay: 2000,
+  enemyShooterDropRate: 0.5,
+  enemyShooterHealth: 2,
+  enemyShooterReward: 500,
+  enemyShooterSpawnDelay: 3000,
+  // Enemy Boss
+  enemyBossBulletDelay: 5000,
+  enemyBossDropRate: 0,
+  enemyBossHealth: 500,
+  enemyBossReward: 5000,
+  // Game level settings
+  height: window.innerHeight * 0.8, // px
+  width: window.innerWidth * 0.8 // px
+};
