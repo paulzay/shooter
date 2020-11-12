227fac6283a724de2936152bff5071c6a0a953fd
paulzay <paulotieno2@gmail.com>
2020-11-12 23:00:30 +0300

    modify the game witdth and height -incomplete

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,21 @@
 import 'phaser';
 
+const ratio = Math.max(window.innerWidth / window.innerHeight, window.innerHeight / window.innerWidth)
+const DEFAULT_HEIGHT = 720 
+const DEFAULT_WIDTH = ratio * DEFAULT_HEIGHT
+
 export default {
     type: Phaser.AUTO,
     parent: 'phaser-example',
-    width: 800,
+    //make it fullscreen/centered
+  // scale: {
+  //   mode: Phaser.Scale.FIT,
+  //   autoCenter: Phaser.Scale.CENTER_BOTH,
+  //   width: DEFAULT_WIDTH,
+  //   height: DEFAULT_HEIGHT
+  // },
+
+	width: 800,
     height: 600,
 	physics: {
 		default: "arcade",
