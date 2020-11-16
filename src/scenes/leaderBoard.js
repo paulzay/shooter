d1321c9a4126fec1190c796b70104fa84ea42e2a
paulzay <paulotieno2@gmail.com>
2020-11-16 17:09:20 +0300

    fix leaderboard to show the scores

--- src/scenes/leaderBoard.js
+++ src/scenes/leaderBoard.js
@@ -9,11 +9,9 @@ export default class LeaderBoard extends Phaser.Scene {
   }
 
   create() {
-    this.cameras.main.setBackgroundColor('#000111');
     getScores().then((scores) => {
-      scores.sort((a, b) => b.score - a.score);
       this.add.text(100, 20, 'RANK  SCORE   NAME');
-      for (let i = 0; i < 5; i += 1) {
+      for (let i = 0; i < 10; i += 1) {
         this.add.text(100, 90 * (i + 1), ` ${i + 1}     ${scores[i].score}   ${scores[i].user}`);
       }
     }).catch(() => {
