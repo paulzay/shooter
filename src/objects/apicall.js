2c74763d0b6249fe46c30f497ae3b879fc813dd6
paulzay <paulotieno2@gmail.com>
2020-11-17 18:04:43 +0300

    fix api bug

--- src/objects/apicall.js
+++ src/objects/apicall.js
@@ -18,26 +18,19 @@ async function postScores(userName, scoreValue) {
   return answer;
 }
 
-function sorting(obj) {
-  const array = [];
-  for (let i = 0; i < obj.length; i += 1) {
-    array.push([obj[i].score, obj[i].user]);
-  }
-  return Array.from(array).sort((a, b) => b[0] - a[0]);
-}
-
 async function getScores() {
-  const settings = {
-    method: 'GET',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-  };
-  const response = await fetch(url, settings);
-  const answer = await response.json();
-
-  return sorting(answer.result);
+  try {
+    const response = await fetch(url, {
+      mode: 'cors',
+    });
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      return jsonResponse.result;
+    }
+    throw new Error('Request Failed!');
+  } catch (error) {
+    return error;
+  }
 }
 
 export { getScores, postScores };
